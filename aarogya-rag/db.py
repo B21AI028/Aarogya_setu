c3837000fbd5d723933d83fcd9ff8b3b24195b15
Renu Sankhla <157734767+B21AI028@users.noreply.github.com>
2025-11-17 20:38:13 +0530

    Update db.py

--- aarogya-rag/db.py
+++ aarogya-rag/db.py
@@ -1,17 +1,3 @@
-# import psycopg2
-# import pandas as pd
-
-# conn = psycopg2.connect(
-#     dbname="aarogya",
-#     user="user",
-#     password="password",
-#     host="localhost",
-#     port="5432"
-# )
-# df = pd.read_sql("SELECT * FROM reports ORDER BY created_at DESC LIMIT 5;", conn)
-# print(df)
-# conn.close()
-# list_tables.py
 import psycopg2
 
 conn = psycopg2.connect(dbname="aarogya", user="user", password="password", host="localhost", port="5432")
